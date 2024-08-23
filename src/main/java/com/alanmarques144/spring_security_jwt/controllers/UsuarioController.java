535ce0e953e5fee050c13cf933c4906a8f2b2bc1
alanmarques144 <alanshalom12@gmail.com>
2024-08-23 19:18:13 -0300

    o controller ja está salvando o usuario no banco de dados

--- src/main/java/com/alanmarques144/spring_security_jwt/controllers/UsuarioController.java
+++ src/main/java/com/alanmarques144/spring_security_jwt/controllers/UsuarioController.java
@@ -1,19 +1,24 @@
 package com.alanmarques144.spring_security_jwt.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alanmarques144.spring_security_jwt.dto.UsuarioDto;
+import com.alanmarques144.spring_security_jwt.service.UsuarioService;
 
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
     
+    @Autowired
+    private UsuarioService usuarioService;
+
     @PostMapping
     private UsuarioDto salvar(@RequestBody UsuarioDto usuarioDto){
-        return usuarioDto;
         
+        return usuarioService.salvar(usuarioDto);
     }
 }
